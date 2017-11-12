912e446c0ad922efbba03f45b31782a541b49bc3
Jp Martins <jphmartins@gmail.com>
2017-11-12 16:01:43 -0200

    Todo-Angular

---
+++ todo-angular/calculadora.js
@@ -0,0 +1,28 @@
+(function () {
+  'use strict'
+
+  angular.module('todoList').controller('calculadoraControl', function ($scope) {
+    $scope.calculo = {valorA: 0, valorB: 0, resultado: ''};
+
+    $scope.somar = function (calculo) {
+      calculo.resultado = parseFloat(calculo.valorA) + parseFloat(calculo.valorB);
+    }
+
+    $scope.dividir = function (calculo) {
+      if (calculo.valorB == 0) {
+        calculo.resultado = 'Impossivel dividir por 0';
+      }else{
+        calculo.resultado = parseFloat(calculo.valorA) / parseFloat(calculo.valorB);
+      }
+    }
+
+    $scope.multiplicar = function (calculo) {
+      calculo.resultado = parseFloat(calculo.valorA) * parseFloat(calculo.valorB);
+    }
+
+    $scope.subtrair = function (calculo) {
+      calculo.resultado = parseFloat(calculo.valorA) - parseFloat(calculo.valorB);
+    }
+
+  });
+})();
